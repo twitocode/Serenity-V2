d756e69a2778d094d7323868aabd0369e7617b5e
TwitoCode <twitodev@gmail.com>
2022-05-07 14:54:17 -0400

    Server - scratch the last commit

--- Backend/Serenity/Modules/Identity/Handlers/LoginUserCommand.cs
+++ Backend/Serenity/Modules/Identity/Handlers/LoginUserCommand.cs
@@ -35,7 +35,7 @@ public class LoginUserCommandHandler : IRequestHandler<LoginUserCommand, Respons
             {
                 Errors = new()
                 {
-                    new("UserNotFoundWithToken", $"Could not find the user with the email of {command.Dto.Email}")
+                    new("UserNotFoundWithEmail", $"Could not find the user with the email of {command.Dto.Email}")
                 },
                 Success = false,
                 Data = null
